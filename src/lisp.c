17d95ea1a4e5f67be98b370165a6611aa365ae94
iTitou <moiandme@gmail.com>
2015-05-14 23:26:33 +0200

    [ref] DRY lisp stubs

--- src/lisp.c
+++ src/lisp.c
@@ -10,6 +10,44 @@ lisp_obj *exit_proc(size_t argc, lisp_obj **argv)
 
 lisp_obj lisp_exit = {.refcount=-42, .type=PROC, .value.p=exit_proc};
 
+/* Some stubs, faster to write directly in lisp */
+static const char *lisp_stubs[] = {
+    "(define (cons head tail)"
+    "  (lambda (pair) (pair head tail)))",
+
+    "(define (car pair)"
+    "  (pair (lambda (head tail) head)))",
+
+    "(define (cdr pair)"
+    "  (pair (lambda (head tail) tail)))",
+
+    "(define (cadr seq) (car (cdr seq)))",
+
+    "(define (caddr seq) (cadr (cdr seq)))",
+
+    "(define (cadddr seq) (caddr (cdr seq)))",
+
+    "(define (range from to)"
+    "  (if (< from to)"
+    "    (cons from (range (+ 1 from) to))"
+    "    #n))",
+
+    "(define (list-get i seq)"
+    "  (if (< i 1) (car seq) (list-get (- i 1) (cdr seq))))",
+
+    "(define (map proc seq)"
+    "  (if seq"
+    "    (cons (proc (car seq)) (map proc (cdr seq)))"
+    "    #n))",
+
+    "(define (reduce proc initial seq)"
+    "  (if seq"
+    "    (proc (car seq) (reduce proc initial (cdr seq)))"
+    "    initial))",
+
+    "(define (apply proc seq) (reduce proc (car seq) (cdr seq)))"
+};
+
 void stdenv(lisp_env *env)
 {
     /* Arithmetic, logical */
@@ -28,36 +66,8 @@ void stdenv(lisp_env *env)
 
     /* Lisp plumbery */
     lisp_obj *res = NULL;
-    res = eval("(define cons (lambda (head tail)"
-               "    (lambda (closure) (closure head tail))))", env, NULL);
-    assert(res == NIL);
-
-    res = eval("(define car (lambda (closure)"
-               "    (closure (lambda (head tail) head))))", env, NULL);
-    assert(res == NIL);
-
-    res = eval("(define cdr (lambda (closure)"
-               "    (closure (lambda (head tail) tail))))", env, NULL);
-    assert(res == NIL);
-
-    res = eval("(define cadr (lambda (lst) (car (cdr lst))))", env, NULL);
-    assert(res == NIL);
-
-    res = eval("(define range (lambda (min max)"
-               "    (if (< min max)"
-               "        (cons min (range (+ 1 min) max))"
-               "        #n)))", env, NULL);
-    assert(res == NIL);
-
-    res = eval("(define map (lambda (proc lst)"
-               "    (if lst"
-               "        (cons (proc (car lst)) (map proc (cdr lst)))"
-               "        #n)))", env, NULL);
-    assert(res == NIL);
-
-    res = eval("(define reduce (lambda (proc lst initial)"
-               "    (if lst"
-               "        (proc (car lst) (reduce proc (cdr lst) initial))"
-               "        initial)))", env, NULL);
-    assert(res == NIL);
+    for (size_t i=0; i<sizeof(lisp_stubs)/sizeof(lisp_stubs[0]); i++){
+        res = eval(lisp_stubs[i], env, NULL);
+        assert(res == NIL);
+    }
 }
