47e03ebf4964358d5ca4f21a7a1517c8425394a5
iTitou <moiandme@gmail.com>
2017-11-27 20:59:56 +0100

    [enh] indent

--- snippets/facrec.lisp
+++ snippets/facrec.lisp
@@ -1,4 +1,8 @@
-(define (facrec acc n) (if (< n 1) acc (facrec (* acc n) (- n 1))))
+(define (facrec acc n)
+    (if (< n 1) 
+        acc 
+        (facrec (* acc n) (- n 1))))
+
 (define (fac n) (facrec (real 1) n))
 
 (fac 150)
